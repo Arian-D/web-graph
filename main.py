56f1cc87c4b22b9fc5fb4c17b0555b81db5b6155
Arian-D <ArianDehghani@protonmail.com>
2020-07-16 11:35:14 -0700

    Added dfs

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 from requests import get
 from sys import argv
 
-LIMIT = 100
+LIMIT = 1000
 
-def search(url: str):
+def search(url: str) -> set:
     queue = []
     edges = set()
     discovered = { url }
@@ -31,15 +31,41 @@ def search(url: str):
                 edges.add((u, href))
     return edges
 
+def dfs_search(url: str) -> set:
+    stack = []
+    edges = set()
+    discovered = { url }
+    stack.append(url) # Push
+    while stack != []:
+        try:
+            u = stack.pop()
+            v = BeautifulSoup(get(u).text, "html.parser")
+        except:
+            return edges
+        if v not in discovered:
+            discovered.add(v)
+            for link in v.findAll('a'):
+                if "href" in link.attrs and link["href"] not in discovered:
+                    href = None
+                    if any(map(link["href"].startswith, ["http://", "https://", "www."])):
+                        href = link["href"]
+                    elif link["href"].startswith('/') and not link["href"].startswith("//"):
+                        href = u + link["href"]
+                    else:
+                        continue
+                    discovered.add(href)
+                    stack.append(href)
+                    edges.add((u, href))
+
 def clean_url(url: str):
     url = url.replace("http://", "").replace("https://", "")
-    return '/'.join(url.split('/')[:2])
+    return '...' + '/'.join(url.split('/')[-2:])
 
 def main():
-    print("digraph {")
+    print("strict digraph {")
     print("\toverlap = false;")
     print("\tsplines = true;")
-    for src, dest in search(argv[1]):
+    for src, dest in dfs_search(argv[1]):
         print(
             '\t"{}" -> "{}";'.format(
                 clean_url(src),
